580cf50bcf8cbc48ef35021f7ad4373153bfddac
Sebastian Wagner <sebastian.wagner@suse.com>
2020-04-06 23:47:36 +0200

    add cache
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- simple-backport-pr.py
+++ simple-backport-pr.py
@@ -12,11 +12,14 @@ Options:
   --ignore-tracker
   -h --help                Show this screen.
 """
+import json
+from datetime import datetime
 from subprocess import check_call, check_output, CalledProcessError
 import os
 import sys
-from typing import List, Tuple
+from typing import List, Tuple, NamedTuple, Dict, Optional
 
+from dateutil import parser
 from github import Github
 from github.Repository import Repository
 from github.PullRequest import PullRequest
@@ -27,6 +30,138 @@ import docopt
 
 check_pr_not_merged = 'pr-not-merged'
 check_tracker = 'tracker'
+
+class GHCache:
+    FNAME = os.path.expanduser('~/.simple-backport-pr.cache.json')
+
+    def __init__(self):
+        try:
+            with open(self.FNAME) as f:
+                self._content: dict = json.load(f)
+        except FileNotFoundError:
+            self._content = {}
+
+        self.pull_instances: Dict[int, PullRequest] = {}
+
+
+    @property
+    def prs(self) -> Dict[str, dict]:
+        if 'prs' not in self._content:
+            self._content['prs'] = {}
+
+        return self._content['prs']
+
+    @property
+    def commits(self) -> Dict[str, dict]:
+        if 'commits' not in self._content:
+            self._content['commits'] = {}
+
+        return self._content['commits']
+
+    @property
+    def pr_commits(self) -> Dict[int, List[str]]:
+        if 'pr_commits' not in self._content:
+            self._content['pr_commits'] = {}
+
+        return self._content['pr_commits']
+
+
+    def save(self):
+        try:
+            c = json.dumps(self._content)
+        except:
+            print(self._content)
+            raise
+        with open(self.FNAME, 'w') as f:
+            f.write(c)
+
+
+
+gh_cache = GHCache()
+
+class CachedCommit(NamedTuple):
+    sha: str
+    message: str
+
+    @classmethod
+    def from_gh(cls, gh: Commit) -> "CachedCommit":
+        r = cls(gh.sha, gh.commit.message)
+        r.save()
+        return r
+
+    @classmethod
+    def from_cache(cls, sha) -> "CachedCommit":
+        d = gh_cache.prs[sha]
+        return cls(**d)
+
+    def save(self):
+        gh_cache.commits[self.sha] = self._asdict()
+        gh_cache.save()
+
+
+class CachedPr(NamedTuple):
+    number: int
+    commits: int
+    title: str
+    body: str
+    merged: bool
+    merged_at: datetime
+    html_url: str
+    ceph: Repository
+
+    @classmethod
+    def from_gh_pr(cls, gh: PullRequest, ceph: Repository) -> "CachedPr":
+        r = cls(gh.number,
+                gh.commits,
+                gh.title,
+                gh.body,
+                gh.merged,
+                gh.merged_at,
+                gh.html_url,
+                ceph)
+        gh_cache.pull_instances[r.number] = gh
+        r.save()
+        return r
+
+
+    @classmethod
+    def from_cache(cls, number: int, ceph: Repository) -> "CachedPr":
+        d = gh_cache.prs[str(number)]
+        d['merged_at'] = parser.isoparse(d['merged_at'])
+        d['ceph'] = ceph
+        if 'html_url' not in d:
+            d['html_url'] = ''
+        return cls(**d)
+
+    def save(self):
+        d = self._asdict()
+        del d['ceph']
+        d['merged_at'] = self.merged_at.isoformat()
+        gh_cache.prs[self.number] = d
+        gh_cache.save()
+
+    def get_commits(self) -> List[CachedCommit]:
+        if self.number in gh_cache.pr_commits:
+            return [
+                CachedCommit.from_cache(sha)
+                for sha in gh_cache.pr_commits[self.number]
+            ]
+        ret = [
+                CachedCommit.from_gh(c) for c in self.github.get_commits()
+            ]
+        gh_cache.pr_commits[self.number] = [cc.sha for cc in ret]
+        gh_cache.save()
+        return ret
+
+
+    @property
+    def github(self) -> PullRequest:
+        if self.number in gh_cache.pull_instances:
+            return gh_cache.pull_instances[self.number]
+        gh_cache.pull_instances[self.number] = self.ceph.get_pull(self.number)
+        return gh_cache.pull_instances[self.number]
+
+
 
 def _check(condition, name, description):
     if condition:
@@ -41,14 +176,14 @@ def get_branch_name(pr_ids: List[str]) ->  str:
     return 'octopus-backport-' + '-'.join(pr_ids)
 
 
-def backport_commits(branch_name: str, commits: List[Commit]):
+def backport_commits(branch_name: str, commits: List[str]):
 
     print(f"git cherry-pick --abort ; git reset --hard HEAD && git checkout octopus && git branch -D {branch_name}")
     
     current_branch = check_output('git symbolic-ref --short HEAD', shell=True).decode().strip()
     assert current_branch == 'octopus', current_branch
 
-    commits_str = ' '.join(c.sha for c in commits)
+    commits_str = ' '.join(c for c in commits)
 
     check_call("git pull upstream octopus", shell=True)
     check_call(f"git checkout -b {branch_name}", shell=True)
@@ -59,8 +194,8 @@ def push_backport_branch(branch_name):
     check_call(f"git push --set-upstream origin {branch_name}", shell=True)
 
 
-def get_pr(ceph, pr_id: str) -> PullRequest:
-    pr: PullRequest = ceph.get_pull(int(pr_id))
+def get_pr(ceph: Repository, pr_id: str) -> CachedPr:
+    pr = CachedPr.from_cache(int(pr_id), ceph)
     _check(not pr.merged,
            check_pr_not_merged,
            f'PR not merged: {pr.html_url}')
@@ -69,16 +204,16 @@ def get_pr(ceph, pr_id: str) -> PullRequest:
            f'looks like pr contains a link to the tracker {pr.html_url}')
     return pr
 
-def get_prs(ceph: Repository, pr_ids: List[str]) -> Tuple[List[PullRequest], List[str]]:
+def get_prs(ceph: Repository, pr_ids: List[str]) -> Tuple[List[CachedPr], List[str]]:
     prs = [get_pr(ceph, pr_id) for pr_id in pr_ids]
     prs = sorted(prs, key=lambda pr: pr.merged_at)
     return prs, [str(pr.number) for pr in prs]
 
 
-def get_pr_commits(pr: PullRequest) -> PaginatedList:
+def get_pr_commits(pr: CachedPr) -> List[CachedCommit]:
     cs = pr.get_commits()
     for c in cs:
-        _check('https://tracker.ceph.com/issues/' in c.commit.message,
+        _check('https://tracker.ceph.com/issues/' in c.message,
                check_tracker,
                f'looks like {c} of {pr} contains a link to the tracker')
 
@@ -87,7 +222,7 @@ def get_pr_commits(pr: PullRequest) -> PaginatedList:
     return cs
 
 
-def commit_in_current_branch(commit: Commit):
+def commit_in_current_branch(commit: CachedCommit):
     try:
         in_branches = [
             b.decode().replace('*', '').strip() for b in
@@ -101,7 +236,7 @@ def commit_in_current_branch(commit: Commit):
         print(f'{commit} already in branch `octopus`')
         sys.exit(3)
 
-    msg = commit.commit.message
+    msg = commit.message
     orig_title = msg.split('\n')[0]
     title=orig_title
     for c in '[]*?':
@@ -117,7 +252,7 @@ def commit_in_current_branch(commit: Commit):
 def create_backport_pull_request(ceph: Repository,
                                  milestone: Milestone,
                                  pr_ids: List[str],
-                                 prs: List[PullRequest],
+                                 prs: List[CachedPr],
                                  title):
     numberstr = ', '.join(f'#{pr_id}' for pr_id in pr_ids)
     body = f"Backport of {numberstr}"
@@ -134,8 +269,8 @@ def create_backport_pull_request(ceph: Repository,
     print(f'Backport PR creted: {backport_pr.html_url}')
 
 
-def get_pr_labels(pr: PullRequest):
-    labels = [l.name for l in pr.labels]
+def get_pr_labels(pr: CachedPr):
+    labels = [l.name for l in pr.github.labels]
     return [l for l in 'cephadm orchestrator mgr documentation'.split() if l in labels]
 
 
@@ -145,7 +280,7 @@ def backport(g: Github, pr_ids: List[str], title:str):
 
     prs, pr_ids = get_prs(ceph, pr_ids)
 
-    commits: List[Commit] = []
+    commits: List[CachedCommit] = []
     for pr in prs:
         this_commits = get_pr_commits(pr)
         for c in this_commits:
@@ -154,9 +289,12 @@ def backport(g: Github, pr_ids: List[str], title:str):
     for c in commits:
         commit_in_current_branch(c)
 
+    commit_shas = [c.sha for c in commits]
+
     branch_name = get_branch_name(pr_ids)
 
-    backport_commits(branch_name, commits)
+    backport_commits(branch_name, commit_shas)
+    sys.exit()
     create_backport_pull_request(ceph,
                                  octopus_milestone,
                                  pr_ids,
@@ -164,13 +302,26 @@ def backport(g: Github, pr_ids: List[str], title:str):
                                  title)
 
 def search_prs(g: Github):
+    ceph: Repository = g.get_repo('ceph/ceph')
     q = {
         'repo': 'ceph/ceph',
         'label':'cephadm',
         'is':'merged',
         'base':'master',
     }
-    #print([pr.number for pr in prs])
+    print(f'requests remaining: {g._Github__requester.rate_limiting[0]}')
+    issues = g.search_issues('', **q)
+    print([issue.number for issue in issues])
+    prs = []
+    for issue in issues[0:20]:
+        try:
+            prs.append(CachedPr.from_cache(int(issue.number), ceph))
+        except:
+            prs.append(CachedPr.from_gh_pr(issue.as_pull_request(), ceph))
+    print(f'requests remaining: {g._Github__requester.rate_limiting[0]}')
+
+
+
 
 def main_create_backport_pr(push: bool,
                             pr_ids: List[str],
@@ -202,6 +353,7 @@ if __name__ == '__main__':
         token = f.read().strip()
 
     g = Github(token)
+    #g = Github()
     if args['search']:
         search_prs(g)
 
